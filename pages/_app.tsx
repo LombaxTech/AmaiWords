58fbb57d48540171124aa92999980ffee5abce45
Rakib <rakib@think-engineer.com>
2024-02-02 23:56:00 +0000

    remove some firebase auth stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { AuthProvider } from "@/context/AuthContext";
+// import { AuthProvider } from "@/context/AuthContext";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
